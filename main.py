f754548f984a98bea65a896dabf1476df73481c6
ali <ali.sadafi.dev@gmail.com>
2023-08-08 17:22:45 +0330

    update: some updates

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import arcade
-
 from game import ChickenInvaders
-
 
 def __main__():
     window = ChickenInvaders()
     window.setup()
-    arcade.run()
+    window.run()
 
 if __name__ == '__main__':
     __main__()
